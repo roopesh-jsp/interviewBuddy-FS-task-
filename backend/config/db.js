9e2ca09157f3a09794eaaa693b3cf09f422301ea
roopesh-jsp <rupzkumar5@gmail.com>
2025-10-25 12:55:39 +0530

    db shifted from sqlite to postgres

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,26 @@
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite", // creates a local file
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./database.sqlite", // creates a local file
+//   logging: false,
+// });
+
+// export default sequelize;
+
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
 });
 
--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5000",
+  // baseURL: "https://render.com/docs/troubleshooting-deploys",
 });
 
 export default apiClient;
